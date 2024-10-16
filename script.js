bf672df5d5c7bbd61967a12f54d5bf5e8012a66d
jacebond007 <136643861+jacebond007@users.noreply.github.com>
2024-10-16 12:20:38 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add JavaScript interactivity here. Example:
+// Alert message when submitting the contact form
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
